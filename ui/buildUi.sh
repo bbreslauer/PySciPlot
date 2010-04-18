40640bf1a098d5e0d37deb5fed3da3d5810c5223
ben <ben@2148b4df-7ce9-4cb8-8841-2ee87decd435>
2010-04-18 17:27:10 +0000

    added buildUi.sh script to automate building .ui files

---
+++ ui/buildUi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# convert all ui files to py source code
+for file in `ls *.ui`
+do
+    ~/usr/local/bin/pyuic4 -o ${file}.py ${file}
+    
+    # remove all connectSlotsByName entries in py files
+    sed '/connectSlotsByName/d' ${file}.py > ${file}.py.new
+    rm ${file}.py
+done
+
+# rename files
+rename -f s/ui.py.new/py/g *.ui.py.new
+
